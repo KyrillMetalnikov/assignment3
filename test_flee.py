757aa8e97349eaf9cb4a0b0bc7036fb3744c9221
Tokeybee <tokeybees@gmail.com>
2020-03-14 16:57:36 -0700

    tested flee function for successful escape

--- test_flee.py
+++ test_flee.py
@@ -16,9 +16,18 @@ class Test(TestCase):
 
     @patch("game.roll_die", side_effect=[1, 2])
     @patch("game.play", side_effect=[""])
-    def test_flee_roll_1_for_print(self, _, __):
+    def test_flee_roll_1_for_hp_change(self, _, __):
         character = {"HP": [5, 5]}
         combat.flee(character)
         actual = character
         expected = {"HP": [3, 5], "in_combat": False}
         self.assertEqual(actual, expected)
+
+    @patch("game.roll_die", side_effect=[3])
+    @patch("sys.stdout", new_callable=io.StringIO)
+    @patch("game.play", side_effect=[""])
+    def test_flee_successful_escape(self, _, mock_sysout, __):
+        character = {"HP": [5, 5]}
+        combat.flee(character)
+        expected = "You managed to falsify a doctors note and got excused from class!\n"
+        self.assertEqual(mock_sysout.getvalue(), expected)
