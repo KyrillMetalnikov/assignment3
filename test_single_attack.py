c5b49eae6ef62c2ae35fddbdd3d3c67c3684a137
Tokeybee <tokeybees@gmail.com>
2020-03-14 16:32:15 -0700

    tested single_attack for miss

--- test_single_attack.py
+++ test_single_attack.py
@@ -28,3 +28,15 @@ Armaan is going crazy and loses 6 sanity!
 
 """
         self.assertEqual(mock_sysout.getvalue(), expected)
+
+    @patch('game.roll_die', side_effect=[5])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_single_attack_miss(self, mock_sysout, _):
+        character = {"HP": [10, 10], "name": "Swag"}
+        enemy = {"HP": [10, 10], "name": "Armaan", "type": 'monster'}
+        combat.single_attack(character, enemy)
+        expected = """Swag sniffled in class!
+Armaan is living in denial! Armaan doesn't lose any sanity!
+
+"""
+        self.assertEqual(mock_sysout.getvalue(), expected)
