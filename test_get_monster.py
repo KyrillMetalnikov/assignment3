e51e1fc0adf2dc0ef6f005d81a7e64255f8bf73c
Tokeybee <tokeybees@gmail.com>
2020-03-14 19:07:47 -0700

    tested get_monster for Chris value.

--- test_get_monster.py
+++ test_get_monster.py
@@ -10,3 +10,10 @@ class Test(TestCase):
         expected = {"HP": [10, 10], 'name': 'Amir', 'attack': 'Amir rolls your name on his magic device!',
                     'type': 'monster'}
         self.assertEqual(actual, expected)
+
+    @patch("random.randint", side_effect=[1])
+    def test_get_monster_chris(self, _):
+        actual = monster.get_monster()
+        expected = {"HP": [1000, 1000], 'name': 'Chris', 'attack': 'Chris says "see?  wasn\'t so hard was it?"',
+            'type': 'monster'}
+        self.assertEqual(actual, expected)
