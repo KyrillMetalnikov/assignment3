20dcbe1dc97ef04ee741d22cfb3dfc85c272ef26
Tokeybee <tokeybees@gmail.com>
2020-03-14 17:43:04 -0700

    tested start_combat for character dying

--- test_start_combat.py
+++ test_start_combat.py
@@ -16,3 +16,15 @@ class Test(TestCase):
         combat.start_combat(character)
         expected = "You outlasted your instructor!\n"
         self.assertEqual(expected, mock_sysout.getvalue())
+
+    @patch('combat.roll_for_initiative', side_effect=[""])
+    @patch('monster.get_monster', side_effect=[""])
+    @patch('game.is_alive', side_effect=[True, True, False, False])
+    @patch('combat.combat_round', side_effect=["", ""])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch('game.play', side_effect=[""])
+    def test_start_combat_death(self, _, mock_sysout, __, ___, ____, _____):
+        character = {"HP": [10, 10]}
+        combat.start_combat(character)
+        expected = "Game over!  You did not manage to survive school.\n"
+        self.assertEqual(expected, mock_sysout.getvalue())
